c71735f6071092315cd0ce887f6a20615fc7fde1
juliadafeo <ullykeit@gmail.com>
2017-10-20 23:51:30 +0400

    Chapter 1. Exercise 1.5

---
+++ exercise_1.5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main()
+{
+    int fahr;
+    
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
+        
+        printf("%3d %6.2f\n", fahr, 5/9.0*(fahr - 32));
+        
+    }
+}
