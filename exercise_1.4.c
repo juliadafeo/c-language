a0569d661996ed6cb0eb27789ee442f41a9d281a
juliadafeo <ullykeit@gmail.com>
2017-10-20 23:36:16 +0400

    Exercise 1.4

---
+++ exercise_1.4.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    float fahr, celsius;
+    int lower, step, upper;
+    
+    lower = -35;
+    step = 5;
+    upper = 40;
+    
+    celsius = lower;
+    
+    printf("%3s %6s\n", "C", "F");
+    
+    while (celsius <= upper) {
+        
+        fahr = 9/5.0*celsius + 32.0;
+        
+        printf("%3.0f %6.1f\n", celsius, fahr);
+        
+        celsius = celsius + step;
+    }
+}
+
+
+
