bbaf38696b937be472b65c60f3cc4f430d94385c
juliadafeo <ullykeit@gmail.com>
2017-10-20 23:38:04 +0400

    Chapter 1. Exercise 1.4

--- exercise_1.4.c
+++ exercise_1.4.c
@@ -8,7 +8,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 #include <stdio.h>
 
-int main()
+void main()
 {
     float fahr, celsius;
     int lower, step, upper;
